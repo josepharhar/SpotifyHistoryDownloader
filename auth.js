ef972c88d892644ab20ed94b7ea6b885b05e2c00
Joseph Arhar <josepharhar@gmail.com>
2017-08-05 21:58:12 +0000

    auth works

--- auth.js
+++ auth.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const secrets = require('./secrets.json');
 const app = express();
 
-app.get('/redirect', (req, res) => {
-  console.log('req: ' + JSON.stringify(req));
+app.get('/auth-redirect', (req, res) => {
+  secrets.authorizationCode = req.query.code;
+  fs.writeFile(path.join(__dirname + '/secrets.json'), JSON.stringify(secrets), (err) => {
+    if (err) {
+      console.error('error writing secrets file: ' + err);
+    } else {
+      console.log('wrote auth code to secrets.json: ' + secrets.authorizationCode);
+    }
+  });
+  res.sendFile(path.join(__dirname + '/auth-redirect.html'));
 });
 
 app.get('/auth', (req, res) => {
   const scopes = 'user-read-recently-played';
-  const redirect_uri = req.headers.host + '/redirect';
+  const redirect_uri = 'http://' + req.headers.host + '/auth-redirect';
   console.log('redirect_uri: ' + redirect_uri);
 
   res.redirect('https://accounts.spotify.com/authorize' + 
@@ -19,6 +29,6 @@ app.get('/auth', (req, res) => {
     '&redirect_uri=' + encodeURIComponent(redirect_uri));
 });
 
-app.listen(process.env.PORT || 4880, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log('auth server started');
 });
