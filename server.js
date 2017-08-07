cafc9f8510eb991311577beeb62a245ed4a04813
Joseph Arhar <josepharhar@gmail.com>
2017-08-06 18:07:41 -0700

    bad request

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const secrets = require('./secrets.json');
+const authorizationCode = secrets.authorizationCode;
 
 var spotifyApi = new SpotifyWebApi({
   clientId: secrets.clientId,
   clientSecret: secrets.clientSecret,
-  redirectUri: 'google.com'
+  redirectUri: ''
 });
 
-spotifyApi.authorizationCodeGrant(secrets.authorizationCode)
+spotifyApi.authorizationCodeGrant(authorizationCode)
   .then(function(data) {
     console.log('Retrieved access token', data.body['access_token']);
-
     spotifyApi.setAccessToken(data.body['access_token']);
 
     return spotifyApi.getMe();
+    // return spotifyApi.getMyRecentlyPlayedTracks();
   })
   .then(function(data) {
     console.log('Retrieved data for ' + data.body['display_name']);
-
     console.log('Email is ' + data.body.email);
-
     console.log('Image URL is ' + data.body.images[0].url);
-
     console.log('This user has a ' + data.body.product + ' account');
   })
   .catch(function(err) {
